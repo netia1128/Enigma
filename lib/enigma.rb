ca8783ebcffdc6dd5d7550d0e241e10cb474ddf4
Netia Ingram <netia1128@hotmail.com>
2021-04-23 12:32:33 -0600

    Add code for #generate_random_key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,23 +2,18 @@ require 'time'
 
 class Enigma
 
-def initialize
-    require 'pry'; binding.pry
-end
+#def encrypt(message, key = need_method_to_generate_random_key, date = Date.now.strftime('%d%m%y'))
+    # The encrypt method takes a message String as an argument. It can optionally take a Key and Date as 
+    # arguments to use for encryption. If the key is not included, generate a random key. 
+    # If the date is not included, use today’s date.
 
-#def encrypt(message, key = need_method_to_generate_random_key, date = )
-# The encrypt method takes a message String as an argument. It can optionally take a Key and Date as arguments 
-# to use for encryption. If the key is not included, generate a random key. If the date is not included, 
-# use today’s date.
-
-# The encrypt method returns a hash with three keys:
-
-# :encryption => the encrypted String
-# :key => the key used for encryption as a String
-# :date => the date used for encryption as a String in the form DDMMYY
+    # The encrypt method returns a hash with three keys:
+        # :encryption => the encrypted String
+        # :key => the key used for encryption as a String
+        # :date => the date used for encryption as a String in the form DDMMYY
 # end
 
-#def decrypt
+#def decrypt(message, key = need_method_to_generate_random_key, date = Date.now.strftime('%d%m%y'))
 # Enigma#decrypt(ciphertext, key, date)
 # The decrypt method takes a ciphertext String and the Key used 
 # for encryption as arguments. The decrypt method can optionally take a date as the third argument. 
@@ -30,5 +25,11 @@ end
 # # :key => the key used for decryption as a String
 # # :date => the date used for decryption as a String in the form DDMMYY
 # end
+
+#move this to a module
+def generate_random_key
+    random_key = rand
+    random_key = random_key.to_s.slice(2..6)
+end
 
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Enigma do
       expect(enigma).to be_a(Enigma)
     end
   end
+
+  #move this to a module
+  describe '#generate_random_key' do
+    it 'can generate a random 5 digit number' do
+      enigma = Enigma.new
+      
+      expect(enigma.generate_random_key.length).to eq(5)
+      expect(enigma.generate_random_key).to be_a(String)
+      expect(enigma.generate_random_key.to_i).to be_between(0, 99_999)
+    end
+  end
 end
