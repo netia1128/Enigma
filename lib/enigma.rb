aae28d36b7311d1bc066e90e4060e08ea00eab6c
Netia Ingram <netia1128@hotmail.com>
2021-04-24 12:00:50 -0600

    Add test and second draft code for enigma #encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,53 +11,30 @@ class Enigma
         range = ('a'..'z').to_a
         range << ''
 
-        message_length = encrypted_message.count
-
-        times_to_loop_hash = {
-            A: (message_length / 4) + (message_length % 4 > 0 ? 1 : 0),
-            B: (message_length / 4) + (message_length % 4 > 1 ? 1 : 0),
-            C: (message_length / 4) + (message_length % 4 > 2 ? 1 : 0),
-            D: (message_length / 4) + (message_length % 4 > 3 ? 1 : 0),
-        }
-
-        count = 0
-        times_to_loop_hash[:A].times do
-            if range.include?(encrypted_message[count]) && encrypted_message[count] != ''
-                index = range.index(encrypted_message[count])
-                altered_range = range.rotate(shift_hash[:A])
-                encrypted_message[count] = altered_range[index]
+        encrypted_message.each_with_index.map do |element, index|
+            if range.include?(encrypted_message[index])
+                case index % 4
+                when 0
+                    range_index = range.index(encrypted_message[index])
+                    altered_range = range.rotate(shift_hash[:A])
+                    encrypted_message[index] = altered_range[range_index]
+                when 1
+                    range_index = range.index(encrypted_message[index])
+                    altered_range = range.rotate(shift_hash[:B])
+                    encrypted_message[index] = altered_range[range_index]
+                when 2
+                    range_index = range.index(encrypted_message[index])
+                    altered_range = range.rotate(shift_hash[:C])
+                    encrypted_message[index] = altered_range[range_index]
+                else
+                    range_index = range.index(encrypted_message[index])
+                    altered_range = range.rotate(shift_hash[:D])
+                    encrypted_message[index] = altered_range[range_index]
+                end
             end
-            count += 4
         end
-        count = 1
-        times_to_loop_hash[:B].times do
-            if range.include?(encrypted_message[count]) && encrypted_message[count]
-                index = range.index(encrypted_message[count])
-                altered_range = range.rotate(shift_hash[:B])
-                encrypted_message[count] = altered_range[index]
-            end
-            count += 4
-        end
-        count = 2
-        times_to_loop_hash[:C].times do
-            if range.include?(encrypted_message[count]) && encrypted_message[count] != ''
-                index = range.index(encrypted_message[count])
-                altered_range = range.rotate(shift_hash[:C])
-                encrypted_message[count] = altered_range[index]
-            end
-            count += 4
-        end
-        count = 3
-        times_to_loop_hash[:D].times do
-            if range.include?(encrypted_message[count]) && encrypted_message[count] != ''
-                index = range.index(encrypted_message[count])
-                altered_range = range.rotate(shift_hash[:D])
-                encrypted_message[count] = altered_range[index]
-            end
-            count += 4
-        end
-
-        require 'pry'; binding.pry
+        
+        encrypted_message = encrypted_message.join
 
         # The encrypt method takes a message String as an argument. It can optionally take a Key and Date as
         # arguments to use for encryption. If the key is not included, generate a random key.
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -16,15 +16,16 @@ RSpec.describe Enigma do
     it 'returns a hash including the encrypted message, key, and date' do
       enigma = Enigma.new
 
-      expect(enigma.encrypt('hello world')).to be_a(Hash)
-      expect(enigma.encrypt('hello world')).to be_a(Hash)
-      expect(enigma.encrypt('hello world')[:key].length).to eq(5)
-      expect(enigma.encrypt('hello world')[:key]).to be_a(String)
-      expect(enigma.encrypt('hello world')[:key].to_i).to be_between(0, 99_999)
-      expect(enigma.encrypt('hello world')[:date].length).to eq(6)
-      expect(enigma.encrypt('hello world')[:date]).to be_a(String)
-      expect(enigma.encrypt('hello world')[:date].slice(0..1).to_i).to be < (32)
-      expect(enigma.encrypt('hello world')[:date].slice(2..3).to_i).to be < (13)
+      # expect(enigma.encrypt('hello world')).to be_a(Hash)
+      # expect(enigma.encrypt('hello world')).to be_a(Hash)
+      expect(enigma.encrypt('hello world.', '02715', '040895')[:message]).to eq('keder ohulw.')
+      # expect(enigma.encrypt('hello world')[:key].length).to eq(5)
+      # expect(enigma.encrypt('hello world')[:key]).to be_a(String)
+      # expect(enigma.encrypt('hello world')[:key].to_i).to be_between(0, 99_999)
+      # expect(enigma.encrypt('hello world')[:date].length).to eq(6)
+      # expect(enigma.encrypt('hello world')[:date]).to be_a(String)
+      # expect(enigma.encrypt('hello world')[:date].slice(0..1).to_i).to be < (32)
+      # expect(enigma.encrypt('hello world')[:date].slice(2..3).to_i).to be < (13)
     end
   end
 
