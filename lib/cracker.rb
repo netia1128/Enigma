d2ee7a6ed308793fee6249ec0df3f57200e5ad41
Netia Ingram <netia1128@hotmail.com>
2021-04-25 10:56:28 -0600

    Add test and code for cracker #determine_shifts_hash_placement_for_cracker_target

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -1,3 +1,31 @@
 class Cracker
-  
+  include Generatable
+  attr_reader :shifts_hash
+
+  def initialize(encrypted_message, date)
+    @encrypted_message = encrypted_message
+    @decrypted_message = ''
+    @cracker_target = encrypted_message.slice(-4..-1).split('')
+    @date = date
+    @range = ('a'..'z').to_a << " "
+    @shifts_hash = {}
+    @shifts_hash_value_arr = [4, 13, 3, 26]
+    @key = ''
+    determine_shifts_hash_placement_for_cracker_target
+  end
+
+  def determine_shifts_hash_placement_for_cracker_target
+    @cracker_target.each do |letter|
+      case @encrypted_message.rindex(letter) % 4
+      when 0
+        @shifts_hash[:A] = letter
+      when 1
+        @shifts_hash[:B] = letter
+      when 2
+        @shifts_hash[:C] = letter
+      else
+        @shifts_hash[:D] = letter
+      end
+    end
+  end
 end
--- spec/cracker_spec.rb
+++ spec/cracker_spec.rb
@@ -7,9 +7,22 @@ require './lib/generatable'
 RSpec.describe Cracker do
   describe '#initialize' do
     it 'exists' do
-      cracker = Cracker.new
+      cracker = Cracker.new('hello world end', '040895')
 
       expect(cracker).to be_a(Cracker)
     end
+    #perhpas a 'has attributes' test here?
+    #but what if i dont need my attributes in my attr_reader?
+  end
+
+  describe '#determine_shifts_hash_placement_for_cracker_target' do
+    it 'determines which shift each letter in the craker target belongs to' do
+      cracker = Cracker.new('hello world end', '040895')
+
+      expect(cracker.shifts_hash.count).to eq(4)
+      expect(cracker.shifts_hash[:A]).to be_a(String)
+    end
+    #perhpas a 'has attributes' test here?
+    #but what if i dont need my attributes in my attr_reader?
   end
 end
