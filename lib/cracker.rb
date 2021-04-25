5b1281751791ee058211c7a6643371ca5c254ae9
Netia Ingram <netia1128@hotmail.com>
2021-04-25 11:56:17 -0600

    Add test and code for cracker #finalize_cracked_shifts_hash

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -9,23 +9,39 @@ class Cracker
     @date = date
     @range = ('a'..'z').to_a << " "
     @shifts_hash = {}
-    @shifts_hash_value_arr = [4, 13, 3, 26]
+    @shifts_hash_key_index = {'0' => [' ', 26] , '1' => ['e', 4], '2' => ['n', 13], '3' => ['d', 3]}
     @key = ''
     determine_shifts_hash_placement_for_cracker_target
   end
+
+  def finalize_cracked_shifts_hash
+    @shifts_hash.each_with_index do |(key, value), index|
+      encrypted_letter_range_index = @range.index(value[0])
+      decrypted_letter_range_index = @shifts_hash_key_index[value[1].to_s][1]
+      @shifts_hash[key] = if encrypted_letter_range_index >= decrypted_letter_range_index
+        encrypted_letter_range_index - decrypted_letter_range_index
+      else
+        altered_range = @range.rotate(encrypted_letter_range_index - decrypted_letter_range_index)
+        altered_range.reverse.index(@shifts_hash_key_index[value[1].to_s][0])
+      end
+    end
+  end
+
+  private
 
   def determine_shifts_hash_placement_for_cracker_target
     @cracker_target.each do |letter|
       case @encrypted_message.rindex(letter) % 4
       when 0
-        @shifts_hash[:A] = letter
+        @shifts_hash[:A] = [letter, @cracker_target.index(letter)]
       when 1
-        @shifts_hash[:B] = letter
+        @shifts_hash[:B] = [letter, @cracker_target.index(letter)]
       when 2
-        @shifts_hash[:C] = letter
+        @shifts_hash[:C] = [letter, @cracker_target.index(letter)]
       else
-        @shifts_hash[:D] = letter
+        @shifts_hash[:D] = [letter, @cracker_target.index(letter)]
       end
     end
+    finalize_cracked_shifts_hash
   end
 end
--- spec/cracker_spec.rb
+++ spec/cracker_spec.rb
@@ -7,7 +7,7 @@ require './lib/generatable'
 RSpec.describe Cracker do
   describe '#initialize' do
     it 'exists' do
-      cracker = Cracker.new('hello world end', '040895')
+      cracker = Cracker.new('keder ohulwthnw', '040895')
 
       expect(cracker).to be_a(Cracker)
     end
@@ -15,14 +15,27 @@ RSpec.describe Cracker do
     #but what if i dont need my attributes in my attr_reader?
   end
 
-  describe '#determine_shifts_hash_placement_for_cracker_target' do
-    it 'determines which shift each letter in the craker target belongs to' do
-      cracker = Cracker.new('hello world end', '040895')
+  #should I test this somehow? Issue is, by the time I call it, the shift hash values have
+  #already changed to the values from #finalize_cracked_shifts_array
+  #ask instructors for help with approach
+  # describe '#determine_shifts_hash_placement_for_cracker_target' do
+  #   it 'determines which shift each letter in the craker target belongs to' do
+  #     cracker = Cracker.new('keder ohulwthnw', '040895')
+
+  #     expect(cracker.shifts_hash.count).to eq(4)
+  #     expect(cracker.shifts_hash[:A]).to be_a(Array)
+  #     expect(cracker.shifts_hash[:A][1]).to be_a(Integer)
+  #     expect(cracker.shifts_hash[:A][0]).to be_a(String)
+  #   end
+  # end
+
+  describe '#finalize_cracked_shifts_hash' do
+    it 'determines final shifts that can be used for decrypting' do
+      cracker = Cracker.new('keder ohulwthnw', '040895')
 
       expect(cracker.shifts_hash.count).to eq(4)
-      expect(cracker.shifts_hash[:A]).to be_a(String)
+      expect(cracker.shifts_hash[:A]).to be_a(Integer)
+      expect(cracker.shifts_hash[:A]).to be < (28)
     end
-    #perhpas a 'has attributes' test here?
-    #but what if i dont need my attributes in my attr_reader?
   end
 end
